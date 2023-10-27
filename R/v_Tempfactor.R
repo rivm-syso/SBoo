e5c5a30a5b4fa52b1ba4fa6b5124af5241b578a7
Joris Quik <joris.quik@rivm.nl>
2023-10-27 15:48:33 +0200

    made air and species specific

--- R/v_Tempfactor.R
+++ R/v_Tempfactor.R
@@ -1,16 +1,45 @@
 #'@title Temperature correction degradation rate water/sed/soil
-#'@name Tempfactor.wsds
+#'@name Tempfactor
 #'@param Q.10 Degradation rate increase factor per /U+00B0 10 C [-]
 #'@param Temp Average precipitation [K]
 #'@return  Temperature correction degradation rate water/sed/soil [-]
 #'@export
 
-Tempfactor <- function(Q.10,Temp, T25, Matrix) {
-
-  if (Matrix %in% c("soil", "sediment", "water")) {
-    return(Q.10^((Temp-T25)/10))
-  } else {
-    return(NA)
+Tempfactor <- function(Q.10,Temp, T25, Ea.OHrad, Matrix, SpeciesName) {
+  
+  if (SpeciesName %in% c("Molecular")) {
+    
+    switch(Matrix,
+           "air" =   {
+             return(exp((Ea.OHrad/constants::syms$r)*((Temp-T25)/T25^2)))
+           },
+           "soil" = {return(Q.10^((Temp-T25)/10))
+           },
+           "sediment" = {return(Q.10^((Temp-T25)/10))
+           },
+           "water" = {return(Q.10^((Temp-T25)/10))
+           },
+           return(NA)
+    )
+  } else { # Particulate
+    
+    switch(Matrix,
+           "air" =   {
+             
+             return(1) # not corrected for temperature or other aspects
+           },
+           "soil" = {
+             return(1) # not corrected for temperature or other aspects
+           },
+           "sediment" = {
+             
+             return(1) # not corrected for temperature or other aspects
+           },
+           "water" = {
+             return(1) # not corrected for temperature or other aspects
+           },
+           return(NA)
+    )
   }
   
 }
