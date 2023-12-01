445fab29a16a78c5d1b6196459500bdc7dff5ca0
Joris Quik <joris.quik@rivm.nl>
2023-12-01 10:28:32 +0100

    add runoff variable for flow and k calculation #12

--- R/k_Runoff.R
+++ R/k_Runoff.R
@@ -5,11 +5,12 @@
 #'@param FRACrun Fraction run-off #[-]
 #'@param RAINrate Average precipitation #[m/s]
 #'@param VertDistance Mixing depth soil #[m]
-#'@param EROSIONsoil Soil erosion #[mm/yr]
 #'@return k_Run-off Run-off of particles from soil to water #[s-1]
 #'@export
 
-k_Runoff <- function(FRACrun, RAINrate, VertDistance, Kscompw,
+k_Runoff <- function(FRACrun, RAINrate, 
+                     Runoff,
+                     Volume, Kscompw,
                      relevant_depth_s, penetration_depth_s,
                      ScaleName, to.SubCompartName, SpeciesName){
   if (ScaleName %in% c("Regional", "Continental") & to.SubCompartName == "sea") {
@@ -20,11 +21,11 @@ k_Runoff <- function(FRACrun, RAINrate, VertDistance, Kscompw,
   } 
   switch(SpeciesName,
          "Molecular" = {
-           ( RAINrate * FRACrun / Kscompw)* 
-             f_CORRsoil(VertDistance, relevant_depth_s, penetration_depth_s) / VertDistance #[s-1]
+           (Runoff / Kscompw) * 
+             f_CORRsoil(VertDistance, relevant_depth_s, penetration_depth_s) / Volume #[s-1]
          },
-         (FRACrun*RAINrate*
-            f_CORRsoil(VertDistance, relevant_depth_s, penetration_depth_s))/VertDistance
+         (Runoff *
+            f_CORRsoil(VertDistance, relevant_depth_s, penetration_depth_s))/ Volume
          #Runoff for particulates should maybe be made dependent on the size of particles that flows freely with water? As opposed to erosion? A function of shear, with a bariere basedon certrain high intensity rain episode?
   )
 }
