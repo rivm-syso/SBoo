41aabd416427e0d7554a0093ac9c46f09b9d1e49
Joris Quik <joris.quik@rivm.nl>
2023-12-01 14:35:04 +0100

    some clean up etc.

--- R/x_ContSea2Moder.R
+++ R/x_ContSea2Moder.R
@@ -5,7 +5,8 @@
 #' @param x_RegSea2Cont flux from regional sea to continental sea
 #' @return water flux; circulation (V/Tau) - sea flux Regional 2 Continental (compensating flux originating from rainwater ??)
 #' @export
-x_ContSea2Moder <- function (Volume, TAUsea, all.x_RegSea2Cont, ScaleName, SubCompartName){
+x_ContSea2Moder <- function (Volume, TAUsea, 
+                             all.x_RegSea2Cont, ScaleName, SubCompartName){
   RegSea2Cont <- all.x_RegSea2Cont$flow[all.x_RegSea2Cont$fromSubCompart == "sea" & 
                                           all.x_RegSea2Cont$fromScale == "Regional"]
   if(ScaleName == "Continental" & SubCompartName == "sea"){
