9ead05a5dbe6077cfd9b638f181d8628dbe60797
Joris Quik <joris.quik@rivm.nl>
2023-12-01 13:44:20 +0100

    Add switch to circumvent calculation for lake #12

--- R/x_RiverDischarge.R
+++ R/x_RiverDischarge.R
@@ -1,21 +1,31 @@
 #' @title RiverDischarge
 #' @name x_RiverDischarge
 #' @param RainRunoff see ppt
-#' @param dischargeFRAC see ppt
-#' @param ContRiver2Reg see ppt
+#' @param dischargeFRAC Fraction discharge between freshwater compartment (w1) at regional and continental scale
+#' @inheritParams x_ContRiver2Reg we need all.x_ContRiver2Reg to obtain a single number, but
+# this way its still (obviously) a flow, to enter tests for fluxes
 #' @return River Discharge 
 #' @export
-x_RiverDischarge <- function (all.Runoff, RainOnFreshwater, dischargeFRAC, all.x_ContRiver2Reg, ScaleName){
-  # twisted, exceptional; we need all.x_ContRiver2Reg to obtain a single number, but\
-  # this way its still (obiously) a flow, to enter tests for fluxes
-  # rain runoff from soils and direct rain on river
+x_RiverDischarge <- function (all.Runoff, RainOnFreshwater, 
+                              dischargeFRAC, all.x_ContRiver2Reg, 
+                              ScaleName, SubCompartName){
+
   x_ContRiver2Reg <- sum(all.x_ContRiver2Reg$flux) #sum to force an atomic number ?
   SumRainRunoff <- sum(all.Runoff$Runoff[all.Runoff$Scale == ScaleName])
-  if(ScaleName == "Continental"){
-    return((SumRainRunoff + RainOnFreshwater) * (1-dischargeFRAC))
-  } 
-  if(ScaleName == "Regional"){
-    return((SumRainRunoff + RainOnFreshwater + x_ContRiver2Reg) * (1-dischargeFRAC))
-  } 
-  else NA
+  
+    switch (SubCompartName,
+    "river" = {
+      if(ScaleName == "Continental"){
+        
+        return((SumRainRunoff + RainOnFreshwater) * (1-dischargeFRAC))
+      } 
+      if(ScaleName == "Regional"){
+        return((SumRainRunoff + RainOnFreshwater + x_ContRiver2Reg) * (1-dischargeFRAC))
+      } 
+      else NA
+    },
+    NA
+  )
+  
+
 }
