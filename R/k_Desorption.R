0c4e96b2d4abc6b90858ff183c3a5c4389e7ae81
Joris Quik <joris.quik@rivm.nl>
2023-12-05 11:35:47 +0100

    Fix in Desoprtion, variables mainly

--- R/k_Desorption.R
+++ R/k_Desorption.R
@@ -1,26 +1,21 @@
-#'@title Desorbtion of particles 
-#'@name Desorption
-#'@param Ksdcompw
-#'@param MTC_2w i.e. sediment
-#'@param MTC_2sd i.e. water
-#'@param VertDistance
+#'@title Desorbtion of molecular species from sediment to water
+#'@name k_Desorption
+#'@param ...
 #'@return Desorption rate constant from sediment to water [s-1]
 #'@export
-k_Desorption <- function (Ksdcompw, MTC_2w, MTC_2sd, VertDistance,
+k_Desorption <- function (Ksdcompw, MTC_2w, to.MTC_2sd, VertDistance,
                           SpeciesName, to.SubCompartName, ScaleName) {
   
   switch (SpeciesName,
-    "molcular" = {
-      if ((ScaleName %in% c("Tropic", "Moderate", "Arctic")) & to.SubCompartName == "sea") {
-        return(NA)
-      }
-      ( (MTC_2sd*MTC_2w)/(MTC_2sd + MTC_2w)/Ksdcompw ) /
+    "Molecular" = {
+      # if ((ScaleName %in% c("Tropic", "Moderate", "Arctic")) & to.SubCompartName == "sea") {
+      #   return(NA)
+      # }
+      ( (to.MTC_2sd*MTC_2w)/(to.MTC_2sd + MTC_2w)/Ksdcompw ) /
         VertDistance
     },
     return(NA)
   )
-  
-
   
 }
 
