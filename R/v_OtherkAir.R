7431b676e3c31ebe84911139321fab5adbd775ac
Joris Quik <joris.quik@rivm.nl>
2023-12-14 11:53:17 +0100

    LoadKaas should  be neglected when using kaas. Work in Progress #11

--- R/v_OtherkAir.R
+++ R/v_OtherkAir.R
@@ -3,10 +3,17 @@
 #' @param all.kaas sneaky way to obtain other loss processes from air
 #' @export
 OtherkAir <- function (all.kaas){
-  OnlyMolecular <- all.kaas$fromSpecies == "Molecular"
+  OnlyMolecular <- all.kaas$fromSpecies == "Unbound" # Unbound and not Molecular as this is not SpeciesName
+  OnlyCalculated <- all.kaas$process != "LoadKaas" # Not sure if this is needed here, but at moment all.kaas consists also of LoadKaas which are doubled with the calculated K's
+  
+  # TODO incorporate OnlyCalculated in selection below
+  
   return(data.frame(
       Scale = all.kaas$fromScale[all.kaas$fromSubCompart == "air" & OnlyMolecular] ,
       Species = all.kaas$fromSpecies[all.kaas$fromSubCompart == "air" & OnlyMolecular],
       OtherkAir = all.kaas$k[all.kaas$fromSubCompart == "air" & OnlyMolecular]
     ))
+  
+  
+  
 }
