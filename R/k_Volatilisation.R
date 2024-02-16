223ec47d4d0064f89d1b92b33a78a67bd818136c
Joris Quik <joris.quik@rivm.nl>
2024-02-16 14:51:33 +0100

    cleanup

--- R/k_Volatilisation.R
+++ R/k_Volatilisation.R
@@ -25,21 +25,3 @@ k_Volatilisation <- function(to.MTC_2w, MTC_2a, to.MTC_2s, Kacompw, FRorig, FRin
   }
 }
 
-# #Volatilisation from soil
-# #partition coefficient; from excel version:
-# #Ksw     (VLOOKUP(ChemClass,QSARtable,7,FALSE))*CORG*RHOsolid/1000
-# #Kp.s1R  (FRorig.s1*Ksw+(1-FRorig.s1)*Ksw.alt)*(1000/RHOsolid/CORG)*CORG.s1R
-# #Ks1w.R  FRACa.s1R*(Kaw.R*FRorig.s1w)+FRACw.s1R+FRACs.s1R*Kp.s1R*RHOsolid/1000
-# #Kaw.R   Kaw*EXP((H0vap/8.314)*(1/298-1/TEMP.R))*EXP(-(H0sol/8.314)*(1/298-1/TEMP.R))*(298/TEMP.R) #but is named KawD.R == v_Kaw()
-# 
-# #Kp(s,sd) = 
-# #Dimensionless soil&sed/water PARTITION COEFFICIENT
-# Ksw = FRACa.s1R*(Kaw.R*FRorig.s1w)+FRACw.s1R+FRACs.s1R*Kp.s1R*RHOsolid/1000
-# Ksdw = FRACw+FRACs*Kp*RHOsolid/1000
-# 
-# #from = s or w; to = air
-# Volat_s = ((from.MTC_2a*to.MTC_2s)/(from.MTC_2a+to.MTC_2s)/((Kaw*FRorig)/Ksw)) * CORRvolat
-# Volat_w = (from.MTC_2a*Kaw.R*FRorig.w1*to.MTC_2w/(from.MTC_2a*Kaw.R*FRorig.w1+to.MTC_2w))*FRw.w0R
-# 
-# SoilPenetratinDepth = 0.1
-# CORRvolat = (EXP((-1/SoilPenetratinDepth)*0)*(1/SoilPenetratinDepth)*DEPTH/(1-EXP((-1/SoilPenetratinDepth)*DEPTH)))
