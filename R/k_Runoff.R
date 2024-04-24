5995fa6ec1430350ec62884589cac5ae197a8f79
Valerie de Rijk <rijkdv@rivm-rsrv-l02p.rivm.ssc-campus.nl>
2024-04-24 11:07:15 +0200

    correction factor runoff

--- R/k_Runoff.R
+++ R/k_Runoff.R
@@ -8,23 +8,24 @@
 #'@return k_Run-off Run-off of particles from soil to water #[s-1]
 #'@export
 
-k_Runoff <- function(Runoff,VertDistance,
+k_Runoff <- function(Runoff,VertDistance, FracROWatComp,
                      Volume, Kscompw,
                      relevant_depth_s, penetration_depth_s,
-                     ScaleName, to.SubCompartName, SpeciesName){
+                     ScaleName, to.SubCompartName, to.ScaleName, SpeciesName, Matrix, all.landFRAC, all.Matrix){
   if (ScaleName %in% c("Regional", "Continental") & to.SubCompartName == "sea") {
     return(NA)
   } 
   if ((ScaleName %in% c("Tropic", "Moderate", "Arctic")) & to.SubCompartName != "sea") {
     return(NA)
   } 
+  #fraction <- FracROWatComp(all.landFRAC, all.Matrix, Matrix, SubCompartName = to.SubCompartName, ScaleName = ScaleName)
   switch(SpeciesName,
          "Molecular" = {
            (Runoff / Kscompw) * 
-             f_CORRsoil(VertDistance, relevant_depth_s, penetration_depth_s) / Volume #[s-1]
+             f_CORRsoil(VertDistance, relevant_depth_s, penetration_depth_s) / Volume  * FracROWatComp (all.landFRAC, all.Matrix, Matrix, SubCompartName = to.SubCompartName, ScaleName) #[s-1]
          },
          (Runoff *
-            f_CORRsoil(VertDistance, relevant_depth_s, penetration_depth_s))/ Volume
+            f_CORRsoil(VertDistance, relevant_depth_s, penetration_depth_s))/ Volume * FracROWatComp (all.landFRAC, all.Matrix, Matrix, SubCompartName = to.SubCompartName, ScaleName)
          #Runoff for particulates should maybe be made dependent on the size of particles that flows freely with water? As opposed to erosion? A function of shear, with a bariere basedon certrain high intensity rain episode?
   )
 }
--- R/v_FracWatComp.R
+++ R/v_FracWatComp.R
@@ -11,28 +11,48 @@
 #
 library(tidyverse)
 
-FracROWatComp <- function (all.landFRAC, all.Matrix, Matrix, SubCompartName, ScaleName) {
-  
-  FracROWatComp4SubCompart <- function(SubCompartName, ScaleName) {
-    
-    compFrac <- all.landFRAC$landFRAC[all.landFRAC$SubCompart == SubCompartName & all.landFRAC$Scale ==  ScaleName]
-    all.landFrac <- as.tibble(all.landFRAC)
-    all.Matrix <- as.tibble(all.Matrix)
-    mergeddata <- left_join(
-      x = all.landFRAC,
-      y = all.Matrix,
-      by = join_by(SubCompart))
+# FracROWatComp <- function (all.landFRAC, all.Matrix, Matrix, SubCompartName, ScaleName) {
+# 
+#   FracROWatComp4SubCompart <- function(SubCompartName, ScaleName) {
+# 
+#     compFrac <- all.landFRAC$landFRAC[all.landFRAC$SubCompart == SubCompartName & all.landFRAC$Scale ==  ScaleName]
+#     all.landFrac <- as.tibble(all.landFRAC)
+#     all.Matrix <- as.tibble(all.Matrix)
+#     mergeddata <- left_join(
+#       x = all.landFRAC,
+#       y = all.Matrix,
+#       by = join_by(SubCompart))
+#     # total landfrac of (fresh) water compartments
+#     waterFrac <- mergeddata |>
+#       filter(Matrix == "water" & Scale == ScaleName) |>
+#       summarise(waterFrac = sum(landFRAC, na.rm = TRUE)) |>
+#       pull(waterFrac)
+#     return ( compFrac / waterFrac)
+# 
+#   }
+#   if ((Matrix == "water") & (ScaleName %in% c("Regional", "Continental"))) {
+#     return(FracROWatComp4SubCompart(SubCompartName, ScaleName))
+#   } else
+#     return(1)
+# }
+
+FracROWatComp <- function(all.landFRAC, all.Matrix, Matrix, SubCompartName, ScaleName) {
+  compFrac <- all.landFRAC$landFRAC[all.landFRAC$SubCompart == SubCompartName & all.landFRAC$Scale ==  ScaleName]
+  all.landFrac <- as.tibble(all.landFRAC)
+  all.Matrix <- as.tibble(all.Matrix)
+  mergeddata <- left_join(
+    x = all.landFRAC,
+    y = all.Matrix,
+    by = join_by(SubCompart))
+
+  if ((Matrix == "water") & (ScaleName %in% c("Regional", "Continental"))) {
     # total landfrac of (fresh) water compartments
     waterFrac <- mergeddata |>
       filter(Matrix == "water" & Scale == ScaleName) |>
       summarise(waterFrac = sum(landFRAC, na.rm = TRUE)) |>
       pull(waterFrac)
-    return ( compFrac / waterFrac)
-    
-  }
-  if ((Matrix == "water") & (ScaleName %in% c("Regional", "Continental"))) {
-    return(FracROWatComp4SubCompart(SubCompartName, ScaleName)) 
-  } else
+    return(compFrac / waterFrac)
+  } else {
     return(1)
+  }
 }
-
