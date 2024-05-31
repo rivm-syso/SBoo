c56d63f047cd4ff1185f91683799b781bc48e8e8
Joris Quik <joris.quik@rivm.nl>
2024-05-31 17:14:44 +0200

    Errosion also needed fix #94

--- R/k_Erosion.R
+++ R/k_Erosion.R
@@ -7,7 +7,9 @@
 #'@param to.SubCompartName name of the subcompartment of the destination box of this process
 #'@return k_Erosion Erosion of attached enp species (P) from soil to water #[s-1]
 #'@export
-k_Erosion <- function(relevant_depth_s,penetration_depth_s, EROSIONsoil, VertDistance, ScaleName, to.SubCompartName,  Matrix, all.landFRAC, all.Matrix ){
+k_Erosion <- function(relevant_depth_s,penetration_depth_s, EROSIONsoil, VertDistance,
+                      to.FracROWatComp,
+                      ScaleName, to.SubCompartName,  Matrix, all.landFRAC, all.Matrix ){
   if (ScaleName %in% c("Regional", "Continental") & to.SubCompartName == "sea") {
     return(NA)
   } 
@@ -15,5 +17,5 @@ k_Erosion <- function(relevant_depth_s,penetration_depth_s, EROSIONsoil, VertDis
     return(NA)
   } 
  # fraction <- FracROWatComp(all.landFRAC, all.Matrix, Matrix, SubCompartName, ScaleName)
-  EROSIONsoil * f_CORRsoil(VertDistance, relevant_depth_s, penetration_depth_s) / VertDistance * FracROWatComp (all.landFRAC, all.Matrix, Matrix, SubCompartName = to.SubCompartName, ScaleName)   #[s-1]
+  EROSIONsoil * f_CORRsoil(VertDistance, relevant_depth_s, penetration_depth_s) / VertDistance * to.FracROWatComp  #[s-1]
 }
