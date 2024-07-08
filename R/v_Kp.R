bdd10d66085087681d69dc6c0ace16e600178516
anne-hids <anne.hids@rivm.nl>
2024-07-08 16:08:27 +0200

    Changed calculation to calculation in Excel

--- R/v_Kp.R
+++ R/v_Kp.R
@@ -15,8 +15,10 @@ Kp <- function(FRorig, KswDorC, Ksw.alt, all.rhoMatrix, Corg, CorgStandard, Matr
     RHOsolid <- all.rhoMatrix$rhoMatrix[all.rhoMatrix$SubCompart == "naturalsoil"]
     return(
       switch (ChemClass,
-        "acid" = (FRorig*KswDorC + (1-FRorig)*Ksw.alt) * (1000 / RHOsolid) * (Corg / CorgStandard),
-        "base" = (FRorig*KswDorC + (1-FRorig)*Ksw.alt) * (1000 / RHOsolid) * (Corg / CorgStandard),
+        #"acid" = (FRorig*KswDorC + (1-FRorig)*Ksw.alt) * (1000 / RHOsolid) * (Corg / CorgStandard), # old calculation
+        #"base" = (FRorig*KswDorC + (1-FRorig)*Ksw.alt) * (1000 / RHOsolid) * (Corg / CorgStandard), # old calculation
+        "acid" = (FRorig*KswDorC + (1-FRorig)*Ksw.alt) * (1000 / RHOsolid / Corg) * CorgStandard,
+        "base" = (FRorig*KswDorC + (1-FRorig)*Ksw.alt) * (1000 / RHOsolid / Corg) * CorgStandard,
         {(FRorig*KswDorC) * (1000 / RHOsolid) * (Corg / CorgStandard)} #Corg
       )
       
