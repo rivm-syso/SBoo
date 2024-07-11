4c43e8ec97653c598174cb1f4a75d018b8e116f5
anne-hids <anne.hids@rivm.nl>
2024-07-11 14:16:42 +0200

    Implemented a test function for k_Adsorption, to temporarily make FRorig and FRorig_spw the same as in excel.

--- R/k_Adsorption.R
+++ R/k_Adsorption.R
@@ -18,17 +18,25 @@ k_Adsorption <- function (FRingas, FRinw, from.MTC_2sd, to.FRorig_spw,
                           to.MTC_2w, from.MTC_2w, to.MTC_2a, from.MTC_2s, to.FRorig, Kacompw, 
                           to.Kscompw, to.Matrix, VertDistance, 
                           AreaLand, AreaSea, to.Area,
-                          from.SubCompartName, to.SubCompartName, ScaleName) {
+                          from.SubCompartName, to.SubCompartName, ScaleName, Test) {
   if ((ScaleName %in% c("Tropic", "Moderate", "Arctic")) & from.SubCompartName == "sea") {
     return(NA)
   }
   switch(to.Matrix,
          
          "water" = { # air to water
+           if (ScaleName %in% c("Regional", "Continental")){
+             if (as.character(Test) == "TRUE"){
+               to.FRorig <- 0.431358608116332
+             } 
+           }
            GASABS = FRingas*(from.MTC_2w*to.MTC_2a/(from.MTC_2w*(Kacompw*to.FRorig)+to.MTC_2a))
            AreaFrac = to.Area/(AreaLand+AreaSea)
            return(GASABS/VertDistance*AreaFrac) },
          "soil" = { # air to soil
+           if (as.character(Test) == "TRUE"){
+             to.FRorig_spw <- 0.00752866498600889
+           } 
            GASABS = FRingas*(from.MTC_2s*to.MTC_2a)/(from.MTC_2s*(Kacompw*to.FRorig_spw)/to.Kscompw+to.MTC_2a)
            AreaFrac = to.Area/(AreaLand+AreaSea)
            return(GASABS/VertDistance*AreaFrac) },
