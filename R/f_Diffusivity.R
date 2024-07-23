357db1d6db9c15d09ad68d215b27b0f926933045
Matthias Hof <hofm@rivm-lsfsv-l13p.rivm.ssc-campus.nl>
2024-07-23 09:38:24 +0200

    Updated parameter descriptions of f_Brown up to f_Diffusivity.
    No changes to the actual functions were made.

--- R/f_Diffusivity.R
+++ R/f_Diffusivity.R
@@ -1,12 +1,12 @@
 #' @title Diffusivity
 #' @name f_Diffusivity
-#' @description Diffusivity
+#' @description Diffusivity, the rate of diffusion [m2/s]
 #' @param Matrix as in c("air", "water", "soil")
-#' @param Temp temperatures
-#' @param DynVisc Dynamic viscosity (air, water)
-#' @param rad_species see rad_species variable
-#' @param Cunningham, see f_Cunningham
-#' @return Diffusivity
+#' @param Temp Temperature [K]
+#' @param DynVisc Dynamic viscosity (air, water) [kg/m/s]
+#' @param rad_species radius of heteroagglomerates [m], calculated in v_rad_species
+#' @param Cunningham, Collision frequency of ENPs with other particulates [-], see f_Cunningham
+#' @return Diffusivity [m2/s]
 #' @export
 f_Diffusivity <- function(Matrix, Temp, DynVisc, rad_species, Cunningham = NULL) {
   if(!is.numeric(Temp)){
