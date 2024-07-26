038dd7a55d3ea8e4bb8cbccc2996c455f266c6b1
Valerie de Rijk <rijkdv@rivm-lsfsv-l12p.rivm.ssc-campus.nl>
2024-07-26 14:54:42 +0200

    x_files documentation

--- R/x_FromModerate2ArctWater.R
+++ R/x_FromModerate2ArctWater.R
@@ -1,6 +1,6 @@
 #' @title Mixing of upper and deeper sea water layers (w2 and w3)
 #' @name x_FromModerate2ArctWater
-#' @param OceanCurrent [m3.s-1]
+#' @param OceanCurrent current implemented in the ocean [m3.s-1]
 #' @param SubCompartName name of the subcompartment of the box at hand
 #' @param ScaleName name of the scale of the box at hand
 #' @return Advection sea - deepocean
