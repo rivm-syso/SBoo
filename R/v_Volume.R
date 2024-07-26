c8e9018c026252034539a21dc02edad76766a729
Valerie de Rijk <rijkdv@rivm-lsfsv-l12p.rivm.ssc-campus.nl>
2024-07-26 14:44:19 +0200

    documentation last v_files

--- R/v_Volume.R
+++ R/v_Volume.R
@@ -1,8 +1,9 @@
 #' @title Volume of the SubCompartment
 #' @name Volume
-#' @param VertDistance hight or depth for subcompartments below the horizon
-#' @param Area in m2
-#' @param FRACcldw fraction of cloudwater
+#' @param VertDistance height or depth for subcompartments below the horizon [m]
+#' @param Area area of the compartment [m2]
+#' @param FRACcldw fraction of cloudwater [-]
+#' @param SubCompartName subcompartment considered
 #' @return Volume
 #' @export
 Volume <- function (VertDistance, Area, FRACcldw, SubCompartName){
--- R/v_rad_species.R
+++ R/v_rad_species.R
@@ -1,23 +1,21 @@
 #' @title Radius of species
 #' @name rad_species
 #' @description Calculate radius of heteroagglomerates [m]
-#' @param SpeciesName description
-#' @param SubCompartName description
+#' @param SpeciesName species considered
+#' @param SubCompartName subcompartment considered
 #' @param NaturalRad natural particle radius all but small in air [m]
 #' @param RadS nanoparticle radius [m]
 #' @param Df fractal dimension of combined heteroagglomerate [-]
 #' @param RadNuc Nucleation mode aerosol particle radius [m]
-#' @param RadCOL Accumulation mode aerosol particle radiusy [m]
+#' @param RadCOL Accumulation mode aerosol particle radius [m]
+#' @param RadCP coarse particulate mode aerosol particle radius [m]
 #' @param NumConcNuc Number concentration of Nucleation mode aerosol particles [#/m3]
 #' @param NumConcAcc Number concentration of Accumulation mode aerosol particles [#/m3]
-#' @param ... dimension(s, Species, Scales, SubCompart) of the
 #' @return rad_species, Approach to calculate the radius of small heteroagglomerates in air/water [m]
 #' @export
 rad_species <- function(SpeciesName,SubCompartName,
                         RadCOL,RadCP,RadNuc,
                         RadS,NumConcNuc,NumConcAcc, Df) {
-  # if(anyNA(c(RadNuc,RadCOL,NumConcNuc,NumConcAcc))) return(NA)
-  # browser()
   switch (tolower(SpeciesName),
           "nanoparticle" = return(RadS),
           "aggregated" = {
@@ -25,7 +23,6 @@ rad_species <- function(SpeciesName,SubCompartName,
               SingleVol <- ((NumConcNuc*(fVol(RadS)+fVol(RadNuc)))+(NumConcAcc*(fVol(RadS)+fVol(RadCOL))))/(NumConcNuc+NumConcAcc)
               rad_particle <- (SingleVol/((4/3)*pi))^(Df)
               return(rad_particle)
-              #return((rad_particle^3 + RadS^3)^(Df))
             } else return((RadCOL^3 + RadS^3)^(Df))
           },
           "attached" = return((RadCP^3 + RadS^3)^(Df)),
