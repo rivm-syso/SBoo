f9286e35fdb74c5c5a1e332a9715969c715e6836
Valerie de Rijk <rijkdv@rivm-lsfsv-l09p.rivm.ssc-campus.nl>
2024-08-07 14:22:59 +0200

    execute back

--- R/R6SBcore.R
+++ R/R6SBcore.R
@@ -164,7 +164,7 @@ SBcore <- R6::R6Class("SBcore",
         stop("emissions should contain Abbr and Emis as columns, and possibly Timed")
       private$solver$PrepemisV(emissions)
       # the solver does the actual work
-      #Solution = private$solver$solvetrial(...)
+      Solution = private$solver$execute(needdebug = F, ...)
       Solution = private$solver$solvetrial(...)
       #private$UpdateDL(Solution)
     },
