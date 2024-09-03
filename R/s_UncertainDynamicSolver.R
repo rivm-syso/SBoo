1a09106436bef5e65ca94052cc344bf971311586
anne-hids <anne.hids@rivm.nl>
2024-09-03 16:30:21 +0200

    Update s_UncertainDynamicSolver.R

--- R/s_UncertainDynamicSolver.R
+++ R/s_UncertainDynamicSolver.R
@@ -10,8 +10,8 @@ UncertainDynamicSolver = function(ParentModule, tmax = 1e10, sample_df, nTIMES =
   
   sample_df <- ParentModule$UncertainInput 
   uniqvNames <- unique(sample_df$varName)
-  nt <- nTIMES
-  
+ 
+  # Create an empty tibble to bind the solutions to in the for loop
   solution <- sample_df |>
     select(varName, Scale, SubCompart) |>
     mutate(Waarde = 0) |>
@@ -29,39 +29,45 @@ UncertainDynamicSolver = function(ParentModule, tmax = 1e10, sample_df, nTIMES =
   solution <- solution[-1,]
   
   vEmissions = ParentModule$emissions
-  
   states = ParentModule$myCore$states$asDataFrame
   
+  # Filter the states to only contain the ones that are in the colnames of SB.K
   SB.K = ParentModule$SB.k
   RowNames <- rownames(SB.K)
   states = states |>
     filter(Abbr %in% RowNames)
   
   for (i in 1:nrow(sample_df$data[[1]])){ 
-    if(is.numeric(vEmissions$Emis)){
-      Emis_df <- vEmissions
+    # First check: if vEmissions is a single emission data frame
+    if(is.numeric(vEmissions$Emis)){                
+      vEmis <- 
+        vEmissions |> 
+        group_by(Abbr) |> 
+        summarise(n=n(),
+                  EmisFun = list(
+                    approxfun(
+                      data.frame(Timed = c(0,Timed), 
+                                 Emis=c(0,Emis)),
+                      rule = 1:2)
+                  )
+        )
+      funlist <- vEmis$EmisFun
+      names(funlist) <- vEmis$Abbr
+      
+    # Second check: if vEmissions is a single set of functions  
+    } else if (class(vEmissions) == "list") {
+      vEmis <- vEmissions
+      
+    # Third check: if vEmissions is a nested tibble with emission data points  
     } else if (inherits(vEmissions$Emis, "list")){
       Abbr <- vEmissions$Abbr
       Emis <- map_dfr(vEmissions$Emis, ~ tibble(Emis = .x$value[i]))
       Emis_df <- cbind(Abbr, Emis)
-    }
-    
-    # Prepare the emissions like in the emission module
-    
-    # Emission preparation for when a df with timed column is given
-    vEmis <- 
-      Emis_df |> 
-      group_by(Abbr) |> 
-      summarise(n=n(),
-                EmisFun = list(
-                  approxfun(
-                    data.frame(Timed = c(0,Timed), 
-                               Emis=c(0,Emis)),
-                    rule = 1:2)
-                )
-      )
-    funlist <- vEmis$EmisFun
-    names(funlist) <- vEmis$Abbr
+      
+    # Fourth check: if vEmissions is a nested tibble with sets of functions
+    } #else if {
+      
+    #}
     
     # Prepare the data to use mutateVar
     df <- sample_df |>
@@ -71,7 +77,7 @@ UncertainDynamicSolver = function(ParentModule, tmax = 1e10, sample_df, nTIMES =
       mutate(Waarde = values)
     ParentModule$myCore$mutateVars(df)
     
-    #Update core
+    # Update core
     ParentModule$myCore$UpdateDirty(uniqvNames)
     ParentModule$PrepKaasM()
     
@@ -81,8 +87,8 @@ UncertainDynamicSolver = function(ParentModule, tmax = 1e10, sample_df, nTIMES =
     SB.m0 <- rep(0, length(SBNames))
     SBtime <- seq(0,tmax,length.out = nTIMES)
     
+    # Define the solver function
     ODEapprox = function(t, m, parms) {
-      #browser()
       with(as.list(c(parms, m)), {
         e <- c(rep(0, length(SBNames)))
         for (name in names(parms$emislist)) {
@@ -101,10 +107,11 @@ UncertainDynamicSolver = function(ParentModule, tmax = 1e10, sample_df, nTIMES =
       parms = list(K = SB.K, SBNames=SBNames, emislist= funlist),
       rtol = 1e-11, atol = 1e-3)
     
+    # Change the colnames of the solved matrix
     colnames(sol)[1:length(SBNames)+1] <- SBNames
     colnames(sol)[grep("signal",colnames(sol))] <- paste("emis",SBNames,sep = "2")
-    #new_colnames <- colnames(sol)
     
+    # Make a tibble in the same format as the 'Solution' tibble
     df <- df |> 
       mutate( 
         varName = ifelse(is.na(varName), "", varName),
@@ -116,16 +123,15 @@ UncertainDynamicSolver = function(ParentModule, tmax = 1e10, sample_df, nTIMES =
       pivot_wider(names_from = new_col_name, values_from = Waarde) |>
       mutate(RUN = i) 
     
-    #sol_tibble <- tibble(sol) |>
-    #  nest(Mass = everything())
+    # Make a tibble from the solved matrix and nest it in a column names Mass
     sol <- tibble(sol) 
-    
     sol_tibble <- tibble(Mass = list(sol))
     
     # Combine result_df and sol_tibble
     final_df <- bind_cols(df, sol_tibble)
     
-    solution <- rbind(solution, final_df)
+    # Bind the new solution to the previous solutions in the solution matrix
+    solution <- bind_rows(solution, final_df)
   }
   
   return(solution)
