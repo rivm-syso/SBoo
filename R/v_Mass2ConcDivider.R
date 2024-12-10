cf948ee128bc2690e986cb7fc65e22cef591f75b
jaap-slootweg <jaap.slootweg@rivm.nl>
2024-12-10 10:14:37 +0100

    debugging Mass2ConcDivider

--- R/v_Mass2ConcDivider.R
+++ R/v_Mass2ConcDivider.R
@@ -1,7 +1,6 @@
 #' @title calculate concentration from mass
 #' @name Mass2Conc
 #' @description calculates Concentrations from mass per Box [g/kg] or [kg/m3]
-#' @param eqMass mass, as returned by solvers [kg]
 #' @param Volume of the Box (state) [m3]
 #' @param rhoMatrix density of the matrix [kg m-3]
 #' @param Fracs fraction of solids in the compartment [-]
@@ -9,17 +8,18 @@
 #' @return Concentration, kg/kg wet for soils, kg/kg wet for sediment, otherwise kg/m3 
 #' @export
 Mass2ConcDivider <- function (Volume, Matrix, all.rhoMatrix, Fracs, Fracw) {
+  #Some SubComparts do not exist?; have Volume NA
+  if (is.na(Volume)) return (NA)
   
   f_Soil.wetweight <- function(Conc.soil, # in kg/m3 soil or sediment
-                               Fracw, Fracs,
-                               RHOsolid){
+                               Fracw, Fracs){
     Conc.soil*1000/(Fracw*RHOw+Fracs*RHOsolid) # in g/kg (wet) soil
   }
 
   if (Matrix %in% c("air", "water")) {
     return(1/Volume)
   } else {
-    RHOsolid <- all.rhoMatrix$rhoMatrix[SubCompart == "othersoil"]
+    RHOsolid <- all.rhoMatrix$rhoMatrix[all.rhoMatrix$SubCompart == "othersoil"]
     RHOw <- all.rhoMatrix$rhoMatrix[all.rhoMatrix$SubCompart == "lake"]
     return(f_Soil.wetweight(1 / Volume, Fracw, Fracs, RHOsolid))
   }
