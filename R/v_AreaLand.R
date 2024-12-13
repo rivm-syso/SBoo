a8b0abaf44ca1e6b52dcef13f4f6c8cf871adf32
Joris Quik <jormercury@users.noreply.github.com>
2024-12-13 16:26:25 +0100

    more generic calculation based on ScaleNestGroup

--- R/v_AreaLand.R
+++ R/v_AreaLand.R
@@ -5,37 +5,46 @@
 #' hence the all. It uses nesting to compute the final value.
 #' @param all.FRACsea    sea fraction of scale, possibly including other scales,
 #' hence the all. It uses nesting to compute the final value.
+#' @param all.ScaleOrder The order from smallest (e.g. local) to largest (e.g. Moderate) scale
+#' @param all.ScaleNestGroup all scales with the same number are nested within each other
 #' @param ScaleName  indicating for which scale the function is called
 #' @return Land in SystemArea
 #' @export
 AreaLand <- function (all.TotalArea,
                       all.FRACsea,
+                      all.ScaleOrder,
+                      all.ScaleNestGroup,
                       ScaleName) {
+  
+  # Data needed for area calculation
+  allData_Scale_Area_Land <- 
+    all.TotalArea |> 
+    full_join(all.FRACsea, by = "Scale") |> 
+    full_join(all.ScaleOrder, by = "Scale") |> 
+    full_join(all.ScaleNestGroup, by = "Scale")
   
   #local function to calculate AreaSea, the naive way (i.e. without "nesting" complications)
   AreaLand4Scale <- function(forScale) {
-    ScaleArea <- all.TotalArea$TotalArea[all.TotalArea$Scale == forScale]
-    ScaleFracLand <- 1 - all.FRACsea$FRACsea[all.FRACsea$Scale == forScale]
-    return(ScaleArea * ScaleFracLand)
+    AreaLandData <- NULL
+    # ScaleArea <- all.TotalArea$TotalArea[all.TotalArea$Scale == forScale]
+    # ScaleFracSea <- all.FRACsea$FRACsea[all.FRACsea$Scale == forScale]
+    for(SNG in unique(all.ScaleNestGroup$ScaleNestGroup)){
+      AreaLandData_SNG <-
+        allData_Scale_Area_Land |> 
+        filter(ScaleNestGroup == SNG) |> 
+        mutate(AreaLand = TotalArea*(1-FRACsea)) |> 
+        mutate(AreaLand_diff = AreaLand - lead(AreaLand, 
+                                             default = 0, 
+                                             order_by = -ScaleOrder))
+      
+      AreaLandData <- rbind(AreaLandData,AreaLandData_SNG)
+    }
+    
+    AreaLandData |> 
+      filter(Scale == forScale) |> 
+      pull(AreaLand_diff)
   }
   
-  if (ScaleName %in% c("Regional", "Arctic")) {
     return(AreaLand4Scale(ScaleName))
-  }
-  
-  if (ScaleName == "Continental") {
-    #TotalArea * FRACLand - (TotalArea * FRACLand) for Regional
-    return(AreaLand4Scale("Continental") - AreaLand4Scale("Regional"))
-  }
-  
-  ContinentalInModerate <- T #For now ! not yet an input option
-  
-  if ((ScaleName == "Moderate" & ContinentalInModerate) |
-      (ScaleName == "Tropic" & !ContinentalInModerate)) {
-    #TotalArea * FRACLand - (TotalArea * FRACLand) for Continental (including Regional)
-    return(AreaLand4Scale(ScaleName) - AreaLand4Scale("Continental"))
-  } else {
-    return(AreaLand4Scale(ScaleName))
-  }
 
 }
