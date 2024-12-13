2cf85884373477d4dd61ddf6ebe81c0a9278a7f7
Joris Quik <jormercury@users.noreply.github.com>
2024-12-13 12:47:42 +0100

    Simplified Area function in process of adding Local Scale + some more documentation

--- R/v_Area.R
+++ R/v_Area.R
@@ -1,18 +1,26 @@
-#' @title Area
+#' @title Calculate Area of each compartment
 #' @name Area
-#' @param AREAland the land area in the considered compartment [m2]
-#' @param AREAsea  the sea area in the considered compartment [m2]
+#' @param AreaLand the land area in the considered compartment [m2]
+#' @param AreaSea  the sea area in the considered compartment [m2]
 #' @param landFRAC fraction of land in the considered compartment [-]
 #' @param SubCompartName the subcompartment which is considered
 #' @param ScaleName the scale which is considered
-#' @return Area (but not for sediment) based on data for the SubCompartment / Scale
+#' @description 
+#' Area for air and cloadwater is set to total area of Land and Sea.
+#' Area of sea, marinesediment and deepocean is set to area of Sea
+#' This is due to the TotalArea being divided in Sea and Land.
+#' Area of Land based compartments are all based on the landFRAC (also freshwater compartments).
+#' landFRAC and FracSea are variables that are commonly changed in combination with TotalArea to change to a new landscape scenario.
+#' Take care of the advective flows as well as they often also need to changed based on the new landscape scenario.
+#' 
+#' @return Area [m2]
 #' @export
 Area <- function (AreaLand,
-                     AreaSea,
-                     landFRAC,
-                     all.landFRAC, 
-                     SubCompartName,
-                     ScaleName) {
+                  AreaSea,
+                  landFRAC,
+                  all.landFRAC, 
+                  SubCompartName,
+                  ScaleName) {
   # easiest
   if (SubCompartName %in% c("air", "cloudwater")) {
     return(AreaLand + AreaSea)
@@ -25,25 +33,26 @@ Area <- function (AreaLand,
       ScaleName %in% c("Arctic", "Moderate", "Tropic")) {
     return(AreaSea)
   }
-  if (SubCompartName == "lakesediment" & ScaleName %in% c("Regional", "Continental")){
-    return(all.landFRAC$landFRAC[all.landFRAC$SubCompart == "lake" & all.landFRAC$Scale == ScaleName] *AreaLand)
-  }
   
-  if (SubCompartName == "freshwatersediment" & ScaleName %in% c("Regional", "Continental")){
-    return(all.landFRAC$landFRAC[all.landFRAC$SubCompart == "river" & all.landFRAC$Scale == ScaleName] *AreaLand)
-  }
-  
+  # if (SubCompartName == "lakesediment" & ScaleName %in% c("Regional", "Continental")){
+  #   return(all.landFRAC$landFRAC[all.landFRAC$SubCompart == "lake" & all.landFRAC$Scale == ScaleName] *AreaLand)
+  # }
+  # 
+  # if (SubCompartName == "freshwatersediment" & ScaleName %in% c("Regional", "Continental")){
+  #   return(all.landFRAC$landFRAC[all.landFRAC$SubCompart == "river" & all.landFRAC$Scale == ScaleName] *AreaLand)
+  # }
+  # 
   
   # on land, lake, freshwater and soils;
-  if (ScaleName %in% c("Regional", "Continental")) {
-    return(landFRAC * AreaLand)
-  }
+  # if (ScaleName %in% c("Regional", "Continental")) {
+  return(landFRAC * AreaLand)
+  # }
   
   # on land, on global scales: only naturalsoil
-  if (SubCompartName == "naturalsoil") {
-    return(AreaLand)
-  }
+  # if (SubCompartName == "naturalsoil") {
+  #   return(AreaLand)
+  # }
   
   #all other cases
-  return(NA)
+  # return(NA) --> These are NA as landFRAC data is NA for all others.
 }
