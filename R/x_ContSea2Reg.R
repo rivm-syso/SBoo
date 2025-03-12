afd97ff1a9d63f8a4a1923e5bbf7102c449fdb4c
Joris Quik <jormercury@users.noreply.github.com>
2025-03-12 18:12:14 +0100

    #201

--- R/x_ContSea2Reg.R
+++ R/x_ContSea2Reg.R
@@ -2,12 +2,11 @@
 #' @name x_ContSea2Reg
 #' @description there is an additional flux from continental sea to regional to compensate from the LakeFracRiver
 #' @param RiverDischarge river discharge rate, see x_RiverDischarge [s-1]
-#' @param LakeFracRiver part of river discharge that originates from lake [-]
 #' @return additional flux from continental sea to regional
 #' @export
-x_ContSea2Reg <- function (all.x_RiverDischarge, LakeFracRiver){
+x_ContSea2Reg <- function (all.x_RiverDischarge){
     #NB the regional river discharge determines the sea flow from continental!!
     x_RiverDischarge <- all.x_RiverDischarge$flow[all.x_RiverDischarge$fromScale=="Regional"]
     
-    return( ((1-LakeFracRiver)/LakeFracRiver) * x_RiverDischarge)
+    return( (10-1) * x_RiverDischarge) # 10-1 based on SB4.01, maybe change to TAU based approach!
 }
