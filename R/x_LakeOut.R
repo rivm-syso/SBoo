afeb4a1cf781f4e20f045e22f61105203f8a764e
Joris Quik <jormercury@users.noreply.github.com>
2025-03-12 17:21:26 +0100

    # 201 is now fixed due to a switch instead of if statement.

--- R/x_LakeOut.R
+++ R/x_LakeOut.R
@@ -1,32 +1,23 @@
 #' @title LakeOut
 #' @name x_LakeOut
-#' @description This calculates the flow from a freshwater compartment within the regional or continental scale to the main freshwater compartment (w1).
+#' @description This calculates the flow from a freshwater (w0) compartment (e.g. lake) within the regional or continental scale to the main freshwater compartment (w1).
 #' @param RainOnFreshwater The direct rain on the freshwater compartment (e.g. a lake).
 #' @param all.Runoff Runoff from soil to the freshwater compartment
-#' @param FracROWatComp The fraction runoff discharging into the seperate w0 freshwater compartment based on surface area fraction.
-#' @param ScaleName different formula for scale Continental or Regional
-#' @return River Discharge 
+#' @param FracROWatComp The fraction runoff discharging into the separate w0 freshwater compartment based on surface area fraction.
+#' @param SubCompartName Only applies to lake compartment, so the compartment name is needed.
+#' @param ScaleName Runoff needs to be calculated per scale.
+#' @return Lake discharge
 #' @export
 x_LakeOut <- function (RainOnFreshwater,
-                           #all.x_RiverDischarge, 
-                           #all.x_ContRiver2Reg,
-                           #LakeFracRiver, 
                            all.Runoff,
                            FracROWatComp,
+                       SubCompartName,
                            ScaleName){
-  # browser()
-  SumRunoff <- sum(all.Runoff$Runoff[all.Runoff$Scale == ScaleName])
-  return(RainOnFreshwater + FracROWatComp*SumRunoff)
-  #x_RiverDischarge <- all.x_RiverDischarge$flow[all.x_RiverDischarge$fromScale==ScaleName]
-  # SumRunoff <- sum(all.Runoff$Runoff[all.Runoff$Scale == ScaleName])
-  # if(ScaleName == "Continental"){
-  #   return(RainOnFreshwater + FracROWatComp*SumRunoff)
-  #   # return(LakeFracRiver * x_RiverDischarge)
-  # } 
-  # if(ScaleName == "Regional"){
-  #   # x_ContRiver2Reg <- all.x_ContRiver2Reg$flow
-  #   # return(LakeFracRiver * (x_RiverDischarge + x_ContRiver2Reg))
-  #   return(RainOnFreshwater + FracROWatComp*SumRunoff)
-  # } 
-  # else NA
+  switch (SubCompartName, # if this is coded with if statement it fails as SubCompartName for Arctic is NA
+          "lake" = {
+            SumRunoff <- sum(all.Runoff$Runoff[all.Runoff$Scale == ScaleName])
+            return(RainOnFreshwater + FracROWatComp*SumRunoff)
+          },
+          NA
+  )
 }
