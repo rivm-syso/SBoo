f11fbb9343ca12db2287c9b11975bb1ace1f8af6
Joris Quik <jormercury@users.noreply.github.com>
2025-03-26 17:19:50 +0100

    Fix in concentration to gram calculation Cleanup Formatting SolverModule

--- R/v_Mass2Conc.R
+++ R/v_Mass2Conc.R
@@ -1,26 +1,24 @@
-#' @title calculate concentration from mass
-#' @name Mass2Conc
-#' @description calculates Concentrations from mass per Box [g/kg] or [kg/m3]
-#' @param eqMass mass, as returned by solvers [kg]
+#' Conversion factor mass to concentration
+#'
+#' Mass2Conc returns the factor to calculate relevant concentrations in reach compartment e.g. to get[kg/kg w] or [kg/m3]
+#'
 #' @param Volume of the Box (state) [m3]
-#' @param rhoMatrix density of the matrix [kg m-3]
-#' @param Fracs fraction of solids in the compartment [-]
-#' @param Fracw fraction of water in compartment [-]
-#' @return Concentration, kg/kg wet for soils, kg/kg wet for sediment, otherwise kg/m3 
+#' @param all.rhoMatrix density of the matrix [kg m-3]
+#' @param FRACa fraction of air in media/matrix [-]
+#' @param FRACw fraction of water in media/matrix [-]
+#' @param Matrix Media or Matrix type
+#' @return Mass2Conc factor
 #' @export
-Mass2Conc <- function (EqMass, Volume, Matrix, all.rhoMatrix, Fracs, Fracw) {
-  
-  f_Soil.wetweight <- function(Conc.soil, # in kg/m3 soil or sediment
-                               Fracw, Fracs,
-                               RHOsolid){
-    Conc.soil*1000/(Fracw*RHOw+Fracs*RHOsolid) # in g/kg (wet) soil
-  }
+Mass2Conc <- function(Volume, SubCompartName, all.rhoMatrix, FRACa, FRACw) {
+  RHOsolid <- all.rhoMatrix$rhoMatrix[all.rhoMatrix$SubCompart == "othersoil"]
+  RHOw <- all.rhoMatrix$rhoMatrix[all.rhoMatrix$SubCompart == "lake"]
 
-  ifelse (Matrix %in% c("air", "water"), 
-          EqMass / Volume, 
-          { RHOsolid <- all.rhoMatrix$rhoMatrix[SubCompart == "othersoil"]
-            RHOw <- all.rhoMatrix$rhoMatrix[all.rhoMatrix$SubCompart == "lake"]
-          f_Soil.wetweight(EqMass / Volume, Fracw, Fracs, RHOsolid)}
+  ifelse(SubCompartName %in% c("air", "cloudwater", "deepocean", "lake", "sea", "river"),
+    1 / Volume,
+    {
+      Fracs <- (1 - FRACw - FRACa)
+      conc <- 1 / Volume
+      conc / (FRACw * RHOw + Fracs * RHOsolid)
+    }
   )
 }
-
