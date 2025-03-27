071a6f3db8d651466b7f1236884eab08ea38b6c9
Joris Quik <jormercury@users.noreply.github.com>
2025-03-27 18:35:37 +0100

    use fcor

--- R/k_Volatilisation.R
+++ R/k_Volatilisation.R
@@ -10,24 +10,18 @@
 k_Volatilisation <- function(to.MTC_2w, from.MTC_2a, to.MTC_2s, Kacompw, from.FRorig, from.FRorig_spw, FRinw, Kscompw,
                              VertDistance, SpeciesName, Matrix, relevant_depth_s, penetration_depth_s,
                              from.SubCompartName, from.ScaleName){ 
-  
-  fromscale <- from.ScaleName
-  
-  fromcompart <- from.SubCompartName
-  
+# browser()
+
   if (SpeciesName %in% c("Molecular")) {
     switch(Matrix,
            "water" = { 
              flux = (to.MTC_2w*from.MTC_2a/(to.MTC_2w*(Kacompw*from.FRorig)+from.MTC_2a))*(Kacompw*from.FRorig)*FRinw
              return(flux/VertDistance)},
            "soil" = { 
-             
              fcor <- f_CORRsoil(VertDistance, relevant_depth_s, penetration_depth_s)
-             flux = (to.MTC_2s*from.MTC_2a)/(to.MTC_2s+from.MTC_2a/((Kacompw*from.FRorig_spw)/Kscompw))*f_CORRsoil(VertDistance, relevant_depth_s, penetration_depth_s)
+             flux = (to.MTC_2s*from.MTC_2a)/(to.MTC_2s+from.MTC_2a/((Kacompw*from.FRorig_spw)/Kscompw))*fcor
              return(flux/VertDistance)}
     )
-    
-    
   } else { 
     return(NA)
   }
