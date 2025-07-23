d6b3009ba9ee7c03659aeb9bb76e0392fcabe88d
Joris Quik <jormercury@users.noreply.github.com>
2025-07-23 14:11:13 +0200

    formatting and description

--- R/v_FracROWatComp.R
+++ R/v_FracROWatComp.R
@@ -1,23 +1,23 @@
-#'@title FracRoWatComp
-#'@name  FracROWatComp
-#'@description Fraction of water component compared to total water in scale for setting fraction of runoff reaching each freshwater compartment
-#'@param all.landFRAC Fractions of land compartments #[-]
-#'@param all.Matrix Matrix compartment the subcompartments belong to #[-]
-#'@param Matrix Current matrix the function calculates for [-]
-#'@param SubCompartName Subcompartment the function calculates for [-] 
-#'@param ScaleName The scale the function calculates for [-]
-#'@return Fraction of water component of .to subcompartment #[-]
-#'@export
+#' @title FracRoWatComp
+#' @name  FracROWatComp
+#' @description Fraction of water component compared to total water in scale for setting fraction of runoff reaching each freshwater compartment
+#' @param all.landFRAC Fractions of land compartments #[-]
+#' @param all.Matrix Matrix compartment the subcompartments belong to #[-]
+#' @param Matrix Current matrix the function calculates for [-]
+#' @param SubCompartName Subcompartment the function calculates for [-]
+#' @param ScaleName The scale the function calculates for [-]
+#' @return Fraction of water component of .to subcompartment #[-]
+#' @export
 #'
-#
+
 
 FracROWatComp <- function(all.landFRAC, all.Matrix, Matrix, SubCompartName, ScaleName) {
   # browser()
 
   if ((Matrix == "water") & (ScaleName %in% c("Regional", "Continental"))) {
-    compFrac <- all.landFRAC$landFRAC[all.landFRAC$SubCompart == SubCompartName & all.landFRAC$Scale ==  ScaleName]
-    mergeddata <- merge(all.landFRAC,all.Matrix)
-    waterFrac = sum(mergeddata$landFRAC[mergeddata$Matrix == "water" & mergeddata$Scale ==  ScaleName])
+    compFrac <- all.landFRAC$landFRAC[all.landFRAC$SubCompart == SubCompartName & all.landFRAC$Scale == ScaleName]
+    mergeddata <- merge(all.landFRAC, all.Matrix)
+    waterFrac <- sum(mergeddata$landFRAC[mergeddata$Matrix == "water" & mergeddata$Scale == ScaleName])
     return(compFrac / waterFrac)
   } else {
     return(NA)
--- R/x_ContRiver2Reg.R
+++ R/x_ContRiver2Reg.R
@@ -5,28 +5,25 @@
 #' @param RunoffFlow RunoffFlow flow from soil to river [m3.s-1]
 #' @param RainOnFreshwater Water flow of rain directly on lake/river [m3.s-1]
 #' @param dischargeFRAC Fraction discharge of freshwater between regional and continental scales and vice versa [-]
-#' @param LakeFracRiver Fraction of river discharge that originates from lake [-]
 #' @return River Discharge for scale Continental [s-1]
 #' @export
-x_ContRiver2Reg <- function (ScaleName, SubCompartName, 
-                             all.RunoffFlow, all.RainOnFreshwater, 
-                             dischargeFRAC){
-  
-  switch (ScaleName,
-          "Continental" = {
-            switch (SubCompartName,
-                    "river" = {
-                      SumRainRunoff <- sum(all.RunoffFlow$RunoffFlow[all.RunoffFlow$Scale == ScaleName])+
-                        sum(all.RainOnFreshwater$RainOnFreshwater[all.RainOnFreshwater$Scale == ScaleName])
-                      # River2sea  <- RainOnFreshwater + SumRunoff * (1-dischargeFRAC)
-                      # Lake2River <- LakeFracRiver * River2sea
-                      
-                      return((SumRainRunoff) * dischargeFRAC)
-                    },
-                    return(NA)
-            )
-          },
-          return(NA)
+x_ContRiver2Reg <- function(ScaleName, SubCompartName,
+                            all.RunoffFlow, all.RainOnFreshwater,
+                            dischargeFRAC) {
+  switch(ScaleName,
+    "Continental" = {
+      switch(SubCompartName,
+        "river" = {
+          SumRainRunoff <- sum(all.RunoffFlow$RunoffFlow[all.RunoffFlow$Scale == ScaleName]) +
+            sum(all.RainOnFreshwater$RainOnFreshwater[all.RainOnFreshwater$Scale == ScaleName])
+          # River2sea  <- RainOnFreshwater + SumRunoff * (1-dischargeFRAC)
+          # Lake2River <- LakeFracRiver * River2sea
+
+          return((SumRainRunoff) * dischargeFRAC)
+        },
+        return(NA)
+      )
+    },
+    return(NA)
   )
-  
 }
