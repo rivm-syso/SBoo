a2060e65f93f604cc01a07f7bf704f44390214d2
Joris Quik <jormercury@users.noreply.github.com>
2025-07-23 10:26:39 +0200

    remove FracROWatComp as influx rain is just related to area

--- R/v_RainOnFreshwater.R
+++ R/v_RainOnFreshwater.R
@@ -3,14 +3,13 @@
 #' @description The fraction of rain falling on lake or river water.
 #' @param RAINrate m.s-1
 #' @param Area in m2
-#' @param FracROWatComp Surface fraction of each water compartment
 #' @param SubCompartName #only for lake/rivers
 #' @return waterflow of rain directly on lake/river
 #' and continental being a part of Moderate (/ Tropic)
 #' @export
-RainOnFreshwater <- function (RAINrate, Area, FracROWatComp, SubCompartName) {
+RainOnFreshwater <- function (RAINrate, Area, SubCompartName) {
   if (SubCompartName %in% c("river", "lake")) {
     # RAINrateToSI is generarted from units !
-    return(RAINrate * Area*FracROWatComp)
+    return(RAINrate * Area)
   } else    return(NA)
 }
