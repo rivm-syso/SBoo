802e0434fa5abf96a0a69930aa67b4d599b0bbc0
Joris Quik <jormercury@users.noreply.github.com>
2025-08-19 15:34:58 +0200

    removed Ksw_orig as parameter in f_Ksw and reformat code in related v function files

--- R/v_KswDorC.R
+++ R/v_KswDorC.R
@@ -4,22 +4,25 @@
 #' @param ChemClass Class of chemical, see QSAR table (REACH, 2012)
 #' @param Kow octanol water partitioning coefficient [-]
 #' @param CorgStandard Standard mass FRACTION organic carbon in soil/sediment [-]
-#' @param a see QSAR table 
-#' @param b see QSAR table 
+#' @param a see QSAR table
+#' @param b see QSAR table
 #' @param rhoMatrix density of the matrix [kg/m3]
 #' @param pKa Dissociation constant of (conjugated) acid (default = 70
 #' @param Ksw soil water partitioning coefficient in data
 #' @export
-KswDorC <- function (Kow, pKa, CorgStandard, ChemClass, a, b, all.rhoMatrix, Ksw){
+KswDorC <- function(Kow, pKa, CorgStandard, ChemClass, a, b, all.rhoMatrix, Ksw) {
   RHOsolid <- all.rhoMatrix$rhoMatrix[all.rhoMatrix$SubCompart == "naturalsoil"]
-  
-  if (is.na(Ksw) || Ksw == "NA") { 
-    if (is.na(pKa) || pKa == "NA"){
+
+  if (is.na(Ksw) || Ksw == "NA") {
+    if (is.na(pKa) || pKa == "NA") {
       pKa <- 7
       warning("pKa is needed but missing, setting pKa=7")
     }
-    f_Ksw(Kow, pKa, CorgStandard , a, b, ChemClass, RHOsolid, FALSE, Ksw)
-    
-  } else return(Ksw)
-  
+    f_Ksw(
+      Kow = Kow, pKa = pKa, CorgStandard = CorgStandard,
+      a = a, b = b, ChemClass = ChemClass, RHOsolid = RHOsolid, alt_form = FALSE
+    )
+  } else {
+    return(Ksw)
+  }
 }
