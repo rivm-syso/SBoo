df5c5eb81896e4a26adb6e9c261e2244fa86510a
Nadim Saadi <nadim.saadi@polymtl.ca>
2026-01-20 14:11:39 -0500

    PlasticFADE - changed deg rate (and settling velocity adjustment) (#238)
    * Added another degradation approach, PlasticFADE, Thanks
    - Now you can set the DegApproach you want to use: Default, Kssdr, PlasticFADE
    
    * Fixes
    - There was a small mistake in the set vel equations (d_eq/2 was used, when it should be shortest_side
    - Some documentation of DragMethod
    - Cleanup of warnings and add source of error messages
    - Add explicit join by to surpress message
    ---------
    Co-authored by Joris Quik

--- R/MSClassicNano.R
+++ R/MSClassicNano.R
@@ -224,13 +224,13 @@ ClassicNanoWorld <- R6::R6Class(
     #check for common field .. can only be matrix
     theMatrix <- names(MatrixSheet)[names(MatrixSheet) %in% names(SubCompartSheet)]
     stopifnot(length(theMatrix) == 1)
-    SubCompartSheet <- left_join(SubCompartSheet, MatrixSheet)
+    SubCompartSheet <- left_join(SubCompartSheet, MatrixSheet, by = "Matrix")
 
     #"inherit" Compartments to SubCompart
     Compartments <- InPutDataFrames[["Compartments"]]
     TheCompartment <- names(Compartments)[names(Compartments) %in% names(SubCompartSheet)]
     stopifnot(length(TheCompartment) == 1)
-    SubCompartSheet <- left_join(SubCompartSheet, Compartments)
+    SubCompartSheet <- left_join(SubCompartSheet, Compartments, by = "Compartment")
     #store the result back into InPutDataFrames
     InPutDataFrames[["SubCompartSheet"]] <- SubCompartSheet
     
