51342753aca46f98898e4514ad9820a27d0cd6c4
anne-hids <anne.hids@rivm.nl>
2026-03-05 16:12:35 +0100

    Update x_RegSea2Cont.R

--- R/x_RegSea2Cont.R
+++ R/x_RegSea2Cont.R
@@ -4,7 +4,7 @@
 #' @param ContSea2Reg find the ContSea2Reg flux in all.(ContSea2Reg)fluxes (there is no direct relation)
 #' @return River Discharge for scale Continental
 #' @export
-x_RegSea2Cont <- function (all.x_RiverDischarge, all.x_ContSea2Regr){
+x_RegSea2Cont <- function (all.x_RiverDischarge, all.x_ContSea2Reg){
   x_RiverDischarge <- all.x_RiverDischarge$flow[all.x_RiverDischarge$fromScale=="Regional"]
   return(all.x_ContSea2Reg$flow + x_RiverDischarge)
 }
