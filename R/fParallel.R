0f829fa0c700cec79dfe75341357b2192eeef207
Joris Quik <jormercury@users.noreply.github.com>
2026-03-25 17:16:36 +0100

    Fix running fakeLib when using initWorldOther.R

--- R/fParallel.R
+++ R/fParallel.R
@@ -79,7 +79,26 @@ solveInParallelSteadyState <- function(max_runs_per_slice,
   processSlice <- function(i) {
     # Load fakelib
     if (!is.null(SBooDataLocation) && !is.na(SBooDataLocation) && SBooDataLocation != "") {
-      source(file.path(SBooDataLocation, "baseScripts/fakeLib.R"))
+      # code take from fakeLib.R in SBooScripts/basescripts
+      library(tidyverse)
+      library(ggdag) #for plotting DAG graphs
+      library(R6)
+      library(rlang)
+      #path to the SBoo package
+      Path2PackageSource <- file.path(SBooDataLocation,"../SBoo")
+      #source all R files and load data from the package
+      Dfiles <- list.files(paste(Path2PackageSource, "data", sep = "/"), pattern = "\\.rda$")
+      Rded <- lapply(Dfiles, function(x) {
+        Dfilename <- paste(Path2PackageSource, "data", x, sep = "/")
+        if (exists("verbose") && verbose) cat(Dfilename, "\n")
+        load(Dfilename, envir = global_env())
+      })
+      Rfiles <- list.files(paste(Path2PackageSource, "R", sep = "/"), pattern = "\\.R$")
+      sourced <- lapply(Rfiles, function(x) {
+        Rfilename <- paste(Path2PackageSource, "R", x, sep = "/")
+        if (exists("verbose") && verbose) cat(Rfilename, "\n")
+        source(Rfilename)
+      })
     } else {
       source("baseScripts/fakeLib.R")}
     
@@ -225,7 +244,26 @@ solveInParallelDynamic <- function(max_runs_per_slice,
   processSlice <- function(i) {
     # Load fakelib
     if (!is.null(SBooDataLocation) && !is.na(SBooDataLocation) && SBooDataLocation != "") {
-      source(file.path(SBooDataLocation, "baseScripts/fakeLib.R"))
+      # code take from fakeLib.R in SBooScripts/basescripts
+      library(tidyverse)
+      library(ggdag) #for plotting DAG graphs
+      library(R6)
+      library(rlang)
+      #path to the SBoo package
+      Path2PackageSource <- file.path(SBooDataLocation,"../SBoo")
+      #source all R files and load data from the package
+      Dfiles <- list.files(paste(Path2PackageSource, "data", sep = "/"), pattern = "\\.rda$")
+      Rded <- lapply(Dfiles, function(x) {
+        Dfilename <- paste(Path2PackageSource, "data", x, sep = "/")
+        if (exists("verbose") && verbose) cat(Dfilename, "\n")
+        load(Dfilename, envir = global_env())
+      })
+      Rfiles <- list.files(paste(Path2PackageSource, "R", sep = "/"), pattern = "\\.R$")
+      sourced <- lapply(Rfiles, function(x) {
+        Rfilename <- paste(Path2PackageSource, "R", x, sep = "/")
+        if (exists("verbose") && verbose) cat(Rfilename, "\n")
+        source(Rfilename)
+      })
     } else {
       source("baseScripts/fakeLib.R")}
     
